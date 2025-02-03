7fc1b8c247a4df735cb841febdbb265624ba05bd
stanislavgerasko <gerasko.s@wb.ru>
2025-02-03 18:36:11 +0300

    review handler

---
+++ internal/handler/review.go
@@ -0,0 +1,67 @@
+package handler
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/ollama/ollama/api"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func ReviewPost(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var request ReviewRequest
+
+	err := decoder.Decode(&request)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	sr := strings.NewReader(request.Text)
+	b := base64.NewDecoder(base64.StdEncoding, sr)
+	var text []byte
+	_, err = b.Read(text)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	client, err := api.ClientFromEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req := &api.GenerateRequest{
+		Model:  "qwen2.5-coder:32b",
+		Prompt: fmt.Sprintf("generate unit test without explanation\n```go\n%s```", text),
+
+		// set streaming to false
+		Stream: new(bool),
+	}
+
+	ctx := context.Background()
+	respFunc := func(resp api.GenerateResponse) error {
+		// Only print the response here; GenerateResponse has a number of other
+		// interesting fields you want to examine.
+		_, err = w.Write([]byte(resp.Response))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		return nil
+	}
+
+	err = client.Generate(ctx, req, respFunc)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+type ReviewRequest struct {
+	Text string `json:"text"`
+}
--- main.go
+++ main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/ollama/ollama/api"
 	"log"
+	"net/http"
+	"reviewllama/internal/handler"
 )
 
 func main() {
-	client, err := api.ClientFromEnvironment()
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/review", handler.ReviewPost)
 
-	req := &api.GenerateRequest{
-		Model:  "qwen2.5-coder:32b",
-		Prompt: "after write example unit test file in golang and check that is all works. after return only go file text",
-
-		// set streaming to false
-		Stream: new(bool),
-	}
-
-	ctx := context.Background()
-	respFunc := func(resp api.GenerateResponse) error {
-		// Only print the response here; GenerateResponse has a number of other
-		// interesting fields you want to examine.
-		fmt.Println(resp.Response)
-		return nil
-	}
-
-	err = client.Generate(ctx, req, respFunc)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
